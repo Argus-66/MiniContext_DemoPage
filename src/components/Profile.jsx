1961c103bc4430b8f48a5e6dd6c7cb08d2dff94d
Ayush H <jqghebri@gmail.com>
2024-08-20 22:33:28 +0530

    creation

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,10 +2,11 @@ import React, {useContext} from 'react'
 import UserContext from '../context/UserContext'
 
 function Profile() {
-    
-  return (
-    <div>Profile</div>
-  )
+    const {user} = useContext(UserContext)
+
+    if (!user) return <div>Please Login</div>
+
+    return <div>Welcome {user.username}</div>
 }
 
 export default Profile
